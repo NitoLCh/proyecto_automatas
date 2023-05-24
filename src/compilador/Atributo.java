ea608be4c120aed448fa59fc34f6d48ac446a381
“David <dpruneda0417@live.com>
2023-05-23 23:38:33 -0600

    Ya puse las acciones de las preposiciones pero me bloque con las de las que invocan (expresion)

--- src/compilador/Atributo.java
+++ src/compilador/Atributo.java
@@ -2,11 +2,19 @@ package compilador;
 public class Atributo {
     public String Lugar;
     public String op;
+    public String siguiente;
+    public String verdadera;
+    public String falsa;
+    public String comienzo;
     
     String tipo = "";
     String h = "";
      public Atributo () {
         Lugar = "";
         op = "";
+        siguiente = "";
+        verdadera = "";
+        falsa = "";
+        comienzo = "";
     }
 }
